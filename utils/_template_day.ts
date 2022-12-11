78d3682800bceb7bfbfa321ec016b273609302bb
Ashin Starr <astarr@seek.com.au>
2022-12-11 14:01:26 +1100

    reformat

--- utils/_template_day.ts
+++
@@ -1,5 +0,0 @@
-import fetchInput from './utils/fetchInput';
-
-const exerciseInput = fetchInput(x);
-
-export const solution = (exerciseInput) => {};
--- utils/fetchInput.ts
+++
@@ -1,2 +0,0 @@
-export default (day: number | string) : Promise<string> => fetch(`https://adventofcode.com/2022/day/${day}/input`)
-    .then(r => r.json() || '');
