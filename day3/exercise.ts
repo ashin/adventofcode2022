2f6875bb26779376875e0e3709464700753f85d6
Ashin Starr <astarr@seek.com.au>
2022-12-12 18:20:45 +1100

    day3 part 2

--- day3/exercise.ts
+++ day3/exercise.ts
@@ -7,15 +7,19 @@ const NEW_LINE = '\n';
 const addNums = (nums: number[]): number => nums.reduce((item, acc) => item + acc, 0);
 
 const solution = (rucksacks: string): number => {
-    const rucksackPriorityValue = rucksacks.split(NEW_LINE).map((rucksack) => {
-        const compartmentSize = Math.floor(rucksack.length / 2);
-        const secondCompartment = rucksack.split('');
-        const firstCompartment = secondCompartment.splice(0, compartmentSize);
-        const rucksackValues = firstCompartment.filter(letter => secondCompartment.indexOf(letter) > -1)
-        return getPriorityValue(rucksackValues[0]);
-    });
+    const rucksackList = rucksacks.split(NEW_LINE);
+    const rucksackGroups: string[][] = [];
+    while(true) {
+        const nextThree = rucksackList.splice(0, 3);
+        rucksackGroups.push(nextThree);
+        if(rucksackList.length < 3) { break; }
+    }
 
-    return addNums(rucksackPriorityValue);
+    const rucksackGroupValues = rucksackGroups.map(rucksackGroup => {
+        const inAllLetters = rucksackGroup[0].split('').find(letter => rucksackGroup[1].includes(letter) && rucksackGroup[2].includes(letter));
+        return inAllLetters ? getPriorityValue(inAllLetters) : 0;
+    });
+    return addNums(rucksackGroupValues);
 };
 
 console.log(solution(exerciseInput));
